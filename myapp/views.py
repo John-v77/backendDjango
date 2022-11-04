21733f5aee1302cd2b23ad00ae90434b3bdde5c8
john-vasile <ionut_vasile85@hotmail.com>
2022-11-04 13:52:43 -0400

    forms in Django

--- myapp/views.py
+++ myapp/views.py
@@ -6,4 +6,11 @@ def index(request):
         'name': 'John',
         'profesion': 'web dev'
     }
-    return render(request, 'index.html', context)
+    return render(request, 'form.html')
+
+
+
+def counter(request):
+    text = request.POST['text']
+    amount_words = len(text.split())
+    return render(request, 'counter.html', {'amount':amount_words})
