943b87a62d704d891045222014956b2c4a8f8edc
john-vasile <ionut_vasile85@hotmail.com>
2022-11-06 13:49:33 -0500

    register view and redirect to login

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('form', views.form, name='form'),
     path('counter', views.counter, name='counter'),
     path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -15,6 +17,36 @@ def counter(request):
     text = request.POST['text']
     amount_words = len(text.split())
     return render(request, 'counter.html', {'amount':amount_words})
+
+
+def register(request):
+    if request.method != 'POST':
+        return render(request, 'register.html')
+
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    if password != password2:
+        messages.info(request, 'Password is not the same')
+        return redirect('register')
+
+    if User.objects.filter(username=username).exists():
+        messages.info(request, 'Username Already Used')
+        return redirect('register')
+    
+    if User.objects.filter(email=email).exists():
+        messages.info(request, 'Email Already Used')
+        return redirect('register')
+    
+    user = User.objects.create_user(username=username, email=email, password=password)
+    user.save()
+    return redirect('login')
+
+def login(request):
+    return render(request, 'login.html')
+
 
 
 
