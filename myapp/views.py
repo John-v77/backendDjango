cc873e222d154754407e7d849e1bb01d2b6ab6e5
john-vasile <ionut_vasile85@hotmail.com>
2022-11-06 19:33:27 -0500

    add login / logout

--- myapp/views.py
+++ myapp/views.py
@@ -45,9 +45,25 @@ def register(request):
     return redirect('login')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+
     return render(request, 'login.html')
 
 
 
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 
